3cc75421d85e623e79e11dcee14b14798757e900
Donghyuk Kim <donghyuk80.kim@gmail.com>
2024-09-12 22:18:59 +0900

    미들웨어 테스트

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
-import Header from '../components/Header'
-import Footer from '../components/Footer'
-import './globals.css'
 import { Metadata } from 'next'
+import './globals.css'
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 
 export const metadata: Metadata = {
   title: {
--- middleware.ts
+++ middleware.ts
@@ -2,12 +2,16 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export async function middleware(request: NextRequest) {
+  console.log(`Middleware executed for: ${request.nextUrl.pathname}`);
+
   const url = request.nextUrl.pathname;
 
   // 현재 호스트를 사용하여 절대 URL 생성
   const protocol = request.nextUrl.protocol;
   const host = request.headers.get('host');
   const apiUrl = `${protocol}//${host}/api/log-visit`;
+
+  console.log(`Middleware apiUrl: ${apiUrl}`);
 
   try {
     // 비동기 처리 await 추가
