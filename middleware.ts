ee168b910ab2666496784b3fa363bdfa6ca97baf
Donghyuk Kim <donghyuk80.kim@gmail.com>
2024-09-12 22:22:40 +0900

    fs 제거

--- middleware.ts
+++ middleware.ts
@@ -4,10 +4,6 @@ import fs from 'fs';
 
 export async function middleware(request: NextRequest) {
   console.log(`Middleware executed for: ${request.nextUrl.pathname}`);
-
-  const logMessage = `Middleware executed for: ${request.nextUrl.pathname}\n`;
-  fs.appendFileSync('/home/ubuntu/log.txt', logMessage);
-
   const url = request.nextUrl.pathname;
 
   // 현재 호스트를 사용하여 절대 URL 생성
